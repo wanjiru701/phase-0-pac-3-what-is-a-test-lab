5583b2356586d3e7df7048a6e2525b96ac588257
whitney <whitney.njogu@student.moringaschool.com>
2022-08-14 19:44:34 +0300

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -3,13 +3,13 @@ const { name, height, message } = require("../index.js");
 describe("what-is-a-test", () => {
   describe("Name", () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+      expect(name).toEqual("Joe");
     });
   });
 
   describe("Height", () => {
     it("is less than 40", () => {
-      expect(height).toBeLessThan(40);
+      expect(height).toBeLessThan(80);
     });
   });
 
